4f758fc6fbb7279a9b09ae0703044eae9da47a53
meirafeden1 <abigya03@gmail.com>
2025-10-23 18:38:57 -0400

    Update app.js CORs

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://candid-gingersnap-50518e.netlify.app", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://candid-gingersnap-50518e.netlify.app"
+    ],
+    credentials: true,
+  })
+);
+
 
 
 // Middleware
